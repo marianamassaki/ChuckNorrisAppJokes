18d491979ed432a1ba145dea0662498908846919
Mariana Eri Massaki <marianamassaki.3@gmail.com>
2022-05-28 03:47:02 -0300

    Alamofire instalado; Criacao das telas, fluxo e componentes; viewControllers criadas;

---
+++ AppJokes/PiadaViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PiadaViewController.swift
+//  AppJokes
+//
+//  Created by Mariana Eri Massaki on 28/05/22.
+//
+
+import UIKit
+
+class PiadaViewController: UIViewController {
+
+    @IBOutlet weak var labelPiadaDetail: UILabel!
+    @IBOutlet weak var imagemDetail: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- AppJokes/ViewController.swift
+++ AppJokes/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagemHome: UIImageView!
+    @IBOutlet weak var piadaHome: UILabel!
+    @IBOutlet weak var shuffleButton: UIButton!
+    @IBOutlet weak var buscarPiadaButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func tappedMyShufflerButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tappedMyBuscarPiadaButton(_ sender: UIButton) {
+    }
+    
 }
 
